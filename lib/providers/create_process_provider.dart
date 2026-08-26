e6f9abc390c0842f4a60663af69e64abd5c9d777
Aleospiria <Alejoospi779@gmail.com>
2026-08-25 23:50:24 -0500

    fix: arreglar response que nos devuelve un http.response en vez de un json.decode

--- lib/providers/create_process_provider.dart
+++ lib/providers/create_process_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sse_frontend_mobil/providers/auth_provider.dart';
 
@@ -11,27 +13,34 @@ class Client {
 
 final clientsProvider = FutureProvider<List<Client>>((ref) async {
   final api = ref.read(apiClientProvider);
-  final data = await api.get('/clients');
-  return (data as List).map((j) => Client.fromJson(j)).toList();
+  final res = await api.get('/clients');
+  final body = jsonDecode(res.body) as List;
+  return body.map((j) => Client.fromJson(j as Map<String, dynamic>)).toList();
 });
 
-final templatesProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
+final templatesProvider =
+    FutureProvider<List<Map<String, dynamic>>>((ref) async {
   final api = ref.read(apiClientProvider);
-  final data = await api.get('/templates');
-  return (data as List).cast<Map<String, dynamic>>();
+  final res = await api.get('/templates');
+  final body = jsonDecode(res.body) as List;
+  return body.cast<Map<String, dynamic>>();
 });
 
 final templateDetailProvider =
     FutureProvider.family<Map<String, dynamic>?, String>((ref, id) async {
   final api = ref.read(apiClientProvider);
-  final data = await api.get('/templates/$id');
-  return data as Map<String, dynamic>?;
+  final res = await api.get('/templates/$id');
+  final body = jsonDecode(res.body);
+  if (body is Map<String, dynamic>) return body;
+  return null;
 });
 
 final nextCodeProvider =
     FutureProvider.family<Map<String, dynamic>?, String>((ref, code) async {
   if (code.isEmpty) return null;
   final api = ref.read(apiClientProvider);
-  final data = await api.get('/process/next-code/$code');
-  return data as Map<String, dynamic>?;
+  final res = await api.get('/process/next-code/$code');
+  final body = jsonDecode(res.body);
+  if (body is Map<String, dynamic>) return body;
+  return null;
 });
